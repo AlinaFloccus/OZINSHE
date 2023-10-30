e6273491dc6c69afa487cf186418c3a6b56c6fb5
Alina.Floccus <a.floccus@gmail.com>
2023-10-30 20:28:19 +0600

    initial commit

---
+++ Ozinshe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ozinshe
+//
+//  Created by Alina Floccus on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
